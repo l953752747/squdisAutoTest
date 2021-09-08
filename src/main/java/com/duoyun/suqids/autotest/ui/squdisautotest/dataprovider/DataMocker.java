321c299e3ee4645fdba0ea61af03fd0d669e2e11
ll <953752747@qq.com>
2021-09-08 18:00:07 +0800

    修改了DataMocker中的注解

--- src/main/java/com/duoyun/suqids/autotest/ui/squdisautotest/dataprovider/DataMocker.java
+++ src/main/java/com/duoyun/suqids/autotest/ui/squdisautotest/dataprovider/DataMocker.java
@@ -2,9 +2,8 @@ package com.duoyun.suqids.autotest.ui.squdisautotest.dataprovider;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-@Service
+
 @Slf4j
 public class DataMocker {
 
--- src/main/java/com/duoyun/suqids/autotest/ui/squdisautotest/dataprovider/config/DataConfig.java
+++ src/main/java/com/duoyun/suqids/autotest/ui/squdisautotest/dataprovider/config/DataConfig.java
@@ -3,7 +3,9 @@ package com.duoyun.suqids.autotest.ui.squdisautotest.dataprovider.config;
 import com.duoyun.suqids.autotest.ui.squdisautotest.dataprovider.DataMocker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DataConfig {
 
     @Value("${browser}")
